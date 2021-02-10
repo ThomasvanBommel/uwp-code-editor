7d3b3f2c494fea4987fe55f113eb167973a8fd50
Thomas vanBommel <thomasvanbommel@hotmail.com>
2021-02-10 10:41:17 -0400

    Initial commit

---
+++ CodeEditor/MainPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace CodeEditor {
+    /// <summary>
+    /// Main page of the code editor
+    /// </summary>
+    public sealed partial class MainPage : Page {
+        public MainPage() {
+            this.InitializeComponent();
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e) {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private async void MyListBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            if (OpenFile.IsSelected) {
+                var picker = new Windows.Storage.Pickers.FileOpenPicker();
+                picker.FileTypeFilter.Add("*");
+                picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+                Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+                if (file != null) {
+                    Results.Text = file.Name;
+                    OpenFile.IsSelected = false;
+                    Explorer.IsSelected = true;
+                }
+            } else if (OpenDir.IsSelected) {
+                var picker = new Windows.Storage.Pickers.FileOpenPicker();
+                picker.FileTypeFilter.Add("*");
+                picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+                Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+
+                if (file != null) {
+                    Results.Text = file.Name;
+                    OpenDir.IsSelected = false;
+                    Explorer.IsSelected = true;
+                }
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+## UWP Code Editor
+
+Editor made using the universal windows platform for class assignment
